ed086570f05d184fc8d4fc85f9b9366a630176a6
lpitto86 <luca.p481@gmail.com>
2023-12-07 20:22:36 +0100

    Finalizzazione

--- js/scripts.js
+++ js/scripts.js
@@ -7,7 +7,27 @@ for (let i = 1; i <= 7; i++) {
     for (let j = i; j <= 100; j += 7) {
         const cell = document.createElement('div');
         cell.className = 'cell';
-        cell.textContent = j;
+
+        // Verifica se è un multiplo
+        if (j % 3 === 0 && j % 5 === 0) {
+            cell.textContent = 'FizzBuzz';
+            cell.style.backgroundColor = '#e74c3c';
+
+            // di 3
+        } else if (j % 3 === 0) {
+            cell.textContent = 'Fizz';
+            cell.style.backgroundColor = '#2ecc71';
+
+            // di 5
+        } else if (j % 5 === 0) {
+            cell.textContent = 'Buzz';
+            cell.style.backgroundColor = '#f1c40f';
+        } else {
+            cell.textContent = j;
+            cell.style.backgroundColor = '#3498db';
+        }
+
+        // Aggiungi la cella alla colonna
         column.appendChild(cell);
     }
 
